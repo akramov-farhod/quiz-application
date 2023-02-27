cbd01a224296109020e6cffabe54f98d87fafcf9
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-02-26 21:52:01 -0700

    added basic layout, starting to work on the functionality

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,1 +1,11 @@
-console.log("hi");
+var startButton = document.getElementById("start-btn");
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  console.log("The quiz has begun!");
+}
+
+function nextQuestion() {}
+
+function selectAnswer() {}
