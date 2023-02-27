99b81fad68a8fb469ae89d8f88e779ca49f9be04
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-02-26 22:43:47 -0700

    functionality is present, time to add questions and start working on the timer

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,13 +1,18 @@
 var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
 var questionContainerElement = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
 var answerButtonsElement = document.getElementById("answer-buttons");
 var shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  nextQuestion();
+});
 
 function startQuiz() {
-  console.log("The quiz has begun!");
+  //   console.log("The quiz has begun!");
   startButton.classList.add("hide");
   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
@@ -33,10 +38,41 @@ function showQuestion(question) {
   });
 }
 function resetState() {
+  clearStatusClass(document.body);
   nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
 }
 
-function selectAnswer() {}
+function selectAnswer(e) {
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Try Again";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 var questions = [
   {
