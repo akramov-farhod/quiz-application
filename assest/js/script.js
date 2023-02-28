8547055d0619581898c4bb8ea71e568e1d08c0a4
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-02-27 23:02:32 -0700

    start and tryagain buttons working propely SOOOO HAPPY

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,6 +1,10 @@
 var leaderboardBtn = document.getElementById("leaderboardBtn");
 var leaderboardSpoiler = document.getElementById("leaderboardSpoiler");
+var startBtn = document.getElementById("startBtn");
+var tryAgainBtn = document.getElementById("tryAgainBtn");
 var leaderboard = document.getElementById("leaderboardWrap");
+var landingText = document.getElementById("landingText");
+var quiz = document.getElementById("quizContent");
 
 leaderboardBtn.addEventListener("click", () => {
   leaderboard.classList.remove("hide");
@@ -8,4 +12,15 @@ leaderboardBtn.addEventListener("click", () => {
 
 leaderboardSpoiler.addEventListener("click", () => {
   leaderboard.classList.add("hide");
+});
+
+startBtn.addEventListener("click", () => {
+  landingText.classList.add("hide");
+  startBtn.classList.add("hide");
+  tryAgainBtn.classList.remove("hide");
+  quiz.classList.remove("hide");
+});
+
+tryAgainBtn.addEventListener("click", () => {
+  document.location.reload();
 });
