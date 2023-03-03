0ac97eae7acbcf77c3ff437bdd6fefa65bf53e03
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-03-02 17:32:48 -0700

    was working on it all day and forgot to do commits, in short, almost everything is working, i just need to create a countdown timer and bind user input to local storage.

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,11 +1,33 @@
+var quizArray;
+
+var apiURL =
+  "https://the-trivia-api.com/api/questions?categories=film_and_tv&limit=10&difficulty=medium";
+var apiCall = fetch(apiURL)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    quizArray = data;
+  });
+
 var leaderboardBtn = document.getElementById("leaderboardBtn");
+var countdown = document.getElementById("countdown");
+var finalScore = document.getElementById("finalScore");
 var leaderboardSpoiler = document.getElementById("leaderboardSpoiler");
 var startBtn = document.getElementById("startBtn");
 var tryAgainBtn = document.getElementById("tryAgainBtn");
 var leaderboard = document.getElementById("leaderboardWrap");
 var landingText = document.getElementById("landingText");
 var quiz = document.getElementById("quizContent");
-var questions = [
+var questionEl = document.getElementById("currentQuestion");
+var answerButtonsEl = document.getElementsByClassName("answer");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+var userInputWrap = document.getElementById("userInputWrap");
+var currentIndex = 0;
+var currentScore = 100;
+var testQuestions = [
   // question 1
   {
     question: "First question of the array",
@@ -63,8 +85,6 @@ var questions = [
   //   question 5
 ];
 
-console.log(questions);
-
 leaderboardBtn.addEventListener("click", () => {
   leaderboard.classList.remove("hide");
 });
@@ -73,11 +93,31 @@ leaderboardSpoiler.addEventListener("click", () => {
   leaderboard.classList.add("hide");
 });
 
+function loadQuestion() {
+  questionEl.innerText = quizArray[currentIndex].question;
+  answer1.innerText = quizArray[currentIndex].incorrectAnswers[0];
+  answer2.innerText = quizArray[currentIndex].correctAnswer;
+  answer3.innerText = quizArray[currentIndex].incorrectAnswers[1];
+  answer4.innerText = quizArray[currentIndex].incorrectAnswers[2];
+  console.log(quizArray[0].question);
+  if (currentIndex < quizArray.length) {
+    currentIndex = currentIndex + 1;
+    console.log(currentIndex);
+  }
+  return;
+}
+
+function score() {
+  countdown.innerText = currentScore;
+  finalScore.innerText = currentScore;
+}
+
 startBtn.addEventListener("click", () => {
   landingText.classList.add("hide");
   startBtn.classList.add("hide");
   tryAgainBtn.classList.remove("hide");
   quiz.classList.remove("hide");
+  loadQuestion();
 });
 
 tryAgainBtn.addEventListener("click", () => {
@@ -87,4 +127,55 @@ tryAgainBtn.addEventListener("click", () => {
   if (areYouSure === true) {
     document.location.reload();
   }
+});
+
+function endOfQuiz() {
+  userInputWrap.classList.remove("hide");
+  quiz.classList.add("hide");
+  score();
+}
+
+answer1.addEventListener("click", () => {
+  if (currentIndex >= quizArray.length) {
+    endOfQuiz();
+    return;
+  }
+  if (!answer1.classList.contains("correct")) {
+    currentScore = currentScore - 10;
+    countdown.innerText = currentScore;
+  }
+  loadQuestion();
+});
+answer2.addEventListener("click", () => {
+  if (currentIndex >= quizArray.length) {
+    endOfQuiz();
+    return;
+  }
+  if (!answer2.classList.contains("correct")) {
+    currentScore = currentScore - 10;
+    countdown.innerText = currentScore;
+  }
+  loadQuestion();
+});
+answer3.addEventListener("click", () => {
+  if (currentIndex >= quizArray.length) {
+    endOfQuiz();
+    return;
+  }
+  if (!answer3.classList.contains("correct")) {
+    currentScore = currentScore - 10;
+    countdown.innerText = currentScore;
+  }
+  loadQuestion();
+});
+answer4.addEventListener("click", () => {
+  if (currentIndex >= quizArray.length) {
+    endOfQuiz();
+    return;
+  }
+  if (!answer4.classList.contains("correct")) {
+    currentScore = currentScore - 10;
+    countdown.innerText = currentScore;
+  }
+  loadQuestion();
 });
